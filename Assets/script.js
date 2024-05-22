d9df1ee2f8f9afe0c45b22c145030a420afcf6ce
Clebio <clebiodasilva285@gmail.com>
2024-05-21 23:22:48 -0300

    js para inserção dos vídeos

--- Assets/script.js
+++ Assets/script.js
@@ -4,3 +4,5 @@ type="text/javascript">
     var j=o.createElement('script');j.type='text/javascript',j.async=true,j.src=i,o.head.appendChild(j);
     var css=o.createElement("link");css.type="text/css",css.rel="stylesheet",css.href=x,o.head.appendChild(css)
   })(window,document, "__sonix","https://sonix.ai/widget.js","https://sonix.ai/widget.css");
+
+  
