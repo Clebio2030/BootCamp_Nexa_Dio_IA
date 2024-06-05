9a26a005700084bb8ef13f755bc5f1d81c2a70cc
Clebio <clebiodasilva285@gmail.com>
2024-06-05 20:17:36 -0300

    page inicial mosaico

---
+++ pages/song4_illusion.js
@@ -0,0 +1,136 @@
+// Música Beatiful Thins - Beason Boone 
+// translation = CÒDIGO DO SONIS
+// Entre as chaves abaixo que irá mandar pro site. 
+
+const songs = 
+[  
+  {
+    "video_url": "nIicRydGZeA",
+    "translation": `<!----- START CODE — SONIX AUDIO EMBEDDABLE MEDIA PLAYER: www.sonix.ai ----->
+            <div class="sonix--embed-container"
+              style="position:relative !important;display:block;overflow-x:hidden;min-height:504px;max-height:504px;overflow-y: hidden;">
+              <div class="sonix--embed-text" style="min-height:504px;max-height:504px;text-align: left;overflow-y: scroll;">
+                <h2 style="font-size:18px;color:#3E3E3B;text-decoration:none;font-weight:700;">Dua Lipa - Illusion (London
+                  Sessions): <a href="https://sonix.ai/?utm_source=embed" style="text-decoration:none;color:#3E3E3B;"
+                    target="_blank" title="Sonix is the best audio transcription service in 2024">Audio automatically
+                    transcribed by Sonix</a></h2>
+                <p style="font-size:14px!important;color:#3E3E3B!important;text-decoration:none!important;display:block;">Dua
+                  Lipa - Illusion (London Sessions): <a
+                    href="https://sonix.ai/speech-to-text-all-supported-file-formats?utm_source=embed"
+                    style="text-decoration:none;color:#3E3E3B;" target="_blank"
+                    title="Sonix&#39;s converts the most popular audio file formats to text">this mp3 audio file </a>was <a
+                    href="https://sonix.ai/transcribe-audio?utm_source=embed" style="text-decoration:none;color:#3E3E3B;"
+                    target="_blank" title="Sonix is the best way to transcribe audio files in 2024">automatically transcribed
+                    by Sonix </a>with the <a href="https://sonix.ai/automated-transcription?utm_source=embed"
+                    style="text-decoration:none;color:#3E3E3B;" target="_blank"
+                    title="The best speech-to-text algorithms">best speech-to-text algorithms. </a>This transcript may contain
+                  errors. </p>
+                <p style="font-size: 14px;color: #3E3E3B;text-decoration: none;"><strong>Dua Lipa: </strong><br />I&#39;ve
+                  been known to miss a red flag. I&#39;ve been known to put my love on a pedestal. In the end, those things
+                  just don&#39;t last. And it&#39;s time I take my rose colored glasses off. I already know you take to me the
+                  things I&#39;d like to see. Maybe you feel like taking me for a ride. Know your time. Think about your kids,
+                  right? Don&#39;t you know I could do this dance all night? Ooh. What you doing? Don&#39;t know who you think
+                  that you&#39;re confusing. I&#39;d be like, ooh. It&#39;s amusing. You think I&#39;m gonna fall for an
+                  illusion?</p>
+                <p style="font-size: 14px;color: #3E3E3B;text-decoration: none;"><strong>Back Vocal: </strong><br />Hey. No,
+                  no. No. Dance. No! I&#39;d be lying. Dance all night. Dance all night. Dance.</p>
+                <p style="font-size: 14px;color: #3E3E3B;text-decoration: none;"><strong>Dua Lipa: </strong><br />This was the
+                  time when that she might have worked. Was the time when I just threw a match and let it burn. Now I&#39;m
+                  grown I know what I deserve I still like dancing with the lessons I already learned I know your type. Two
+                  things I like truly make me yours for life. Take me for a ride. Enjoy your time. Think about your country.
+                  Don&#39;t you know I could do this dance all night? Ooh. What you doing? Don&#39;t know. Who do you think
+                  that you&#39;re confusing? I&#39;d be like, ooh, it&#39;s amusing. You think I&#39;m gonna.</p>
+                <p style="font-size: 14px;color: #3E3E3B;text-decoration: none;"><strong>Dua Lipa: </strong><br />Fall for an
+                  aloof. Oh, what a shame. Hey, dance all night. Have you left it so late? Dance all night. Dance all night.
+                  Is this.</p>
+                <p style="font-size: 14px;color: #3E3E3B;text-decoration: none;"><strong>Dua Lipa: </strong><br />Losing? I
+                  really like the way you&#39;re moving. Yeah. I just wanna dance with the loser. Yeah I just wanna dance
+                  with. Yeah, I just wanna dance with I could dance all night. Hey. So what you do when you don&#39;t know who
+                  you think that you&#39;re confusing? I&#39;d be like, ooh, it&#39;s amusing.</p>
+                <p style="font-size: 14px;color: #3E3E3B;text-decoration: none;"><strong>Dua Lipa: </strong><br />You think
+                  I&#39;m gonna fall for it? Oh. What you.</p>
+                <p style="font-size: 14px;color: #3E3E3B;text-decoration: none;"><strong>Dua Lipa: </strong><br />Doing?
+                  Don&#39;t know who you think that you&#39;re confusing. I&#39;d be like, ooh, it&#39;s amusing. You think
+                  I&#39;m grateful for it? Or do. Asian.</p>
+                <h3 style="font-size: 14px;line-height: 18px;color: #3E3E3B;text-decoration: none;display:inline;"><a
+                    href="https://sonix.ai/?utm_source=embed" style="font-size: 14px;color: #3E3E3B;text-decoration: none;"
+                    target="_blank" title="Sonix is the best automated transcription service online">Sonix </a>is the world’s
+                  most advanced <a href="https://sonix.ai/automated-transcription?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Best automated transcription service">automated transcription, </a><a
+                    href="https://sonix.ai/translation?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Accurate automated translation with Sonix">translation, and </a><a
+                    href="https://sonix.ai/automated-subtitles-and-captions?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Easily create and edit subtitles with Sonix">subtitling platform. </a><a
+                    href="https://sonix.ai/fast-transcription?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Fast automated transcription &amp; translation">Fast, </a><a
+                    href="https://sonix.ai/automated-transcription?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Accurate automated transcription &amp; translation">accurate, </a>and <a
+                    href="https://sonix.ai/pricing?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Affordable automated transcription &amp; translation">affordable. </a></h3>
+                <h4 style="font-size:14px!important;color:#3E3E3B!important;text-decoration:none!important;display:inline;"><a
+                    href="https://sonix.ai/best-mp3-to-text-converter?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Automatically convert your mp3 files to text">Automatically convert your mp3 files to text </a><a
+                    href="https://sonix.ai/how-to-convert-mp3-to-text?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Accurately convert mp3 to a txt file with Sonix">(txt file), </a><a
+                    href="https://sonix.ai/how-to-convert-mp3-to-docx?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Accurately convert mp3 to docx with Sonix">Microsoft Word (docx file), </a>and <a
+                    href="https://sonix.ai/how-to-convert-mp3-to-srt?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Accurately convert mp3 to srt with Sonix">SubRip Subtitle (srt file) </a>in minutes. </h4>
+                <p style="font-size:14px!important;color:#3E3E3B!important;text-decoration:none!important;display:inline;"><a
+                    href="https://sonix.ai/" style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Best automated transcription, translation, and subtitling service">Sonix </a>has many features that
+                  you'd love including <a style="font-size: 14px;color: #3E3E3B;text-decoration: none;"
+                    title="powerful integrations and APIs" target="_blank"
+                    href="https://sonix.ai/features/integrations?utm_source=embed">powerful integrations and APIs</a>, <a
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" title="transcribe multiple languages"
+                    target="_blank" href="https://sonix.ai/languages?utm_source=embed">transcribe multiple languages</a>, <a
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" title="world-class support" target="_blank"
+                    href="https://sonix.ai/features/world-class-support?utm_source=embed">world-class support</a>, <a
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" title="upload many different filetypes"
+                    target="_blank" href="https://sonix.ai/list-of-languages-and-filetypes?utm_source=embed">upload many
+                    different filetypes</a>, and <a href="https://sonix.ai/how-to-transcribe-a-zoom-meeting?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Quickly and accurately transcribe your Zoom meetings and recordings with Sonix">easily transcribe
+                    your Zoom meetings. </a><a href="https://sonix.ai/accounts/sign_up?utm_source=embed"
+                    style="font-size: 14px;color: #3E3E3B;text-decoration: none;" target="_blank"
+                    title="Try Sonix for free today">Try Sonix for free today. </a>
+      
+                </p>
+              </div>
+              <div class="sonix--embeddable sonix-embed" data-disable-analytics="false"
+                data-sonix-id="PQAnzAriGHv656LyaPHFWPA8"
+                style="position: absolute !important;top:0 !important;bottom: 0 !important;left: 0 !important; right: 0!important;">
+                <div class="sonix--javascript"></div>
+              </div>
+            </div>
+            <!----- END CODE — SONIX AUDIO EMBEDDABLE MEDIA PLAYER: www.sonix.ai ----->`
+            }
+    ]
+
+songs.forEach((song)=>{
+    var song_container=document.createElement("div")
+    song_container.classList.add('song-container')
+    song_container.innerHTML=`
+    <div class="song-translation">
+    ${song.translation}
+    </div>
+    <div class="song-video">
+    <iframe src="https://www.youtube.com/embed/${song.video_url}" title="YouTube video player"
+      frameborder="0"
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+  </div>
+    `
+    var main=document.querySelector('main')
+    main.appendChild(song_container)
+})
